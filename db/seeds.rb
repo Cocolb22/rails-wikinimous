a32c9b539e5301ed35d01537707c6c9f03e93785
Corentin Le Bris <corentin.lebris@hotmail.fr>
2022-12-08 16:01:05 +0100

    create CRUD, views and front

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,28 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+
+# # # # # # # #  NEWS ARTICLES  # # # # # # # #
+
+first_article = Article.new(title: "la Zone 51, zone touristique",
+                            content:"Des aliens repétés en train de se balader et taper un foot dans la zone")
+first_article.save!
+
+second_article = Article.new(title: "le mur de Trump en démolition",
+                            content: " Après toutes les conneries dites et faites,
+                            Donald a été condamnné à démoter toutes les portions du mur à la frontière entre le MExique et les USA ")
+second_article.save!
+third_article = Article.new(title:"Nolween Le Roy perdue dans la fôret de Brocéliande",
+                            content:" Après avoir trop chanté 'j'entends le loup le renard et la belette' et bu trop de potion,
+                            la chanteuse s'est égaré dans la fôret. Les recherches sont toujours en cours")
+third_article.save!
+fourth_article = Article.new(title:"Neymar roule toujours sur le bord du terrain",
+                            content:"Après avoir été effleuré par
+                              une guêpe lors d'un match, le joueur se roule depuis maintenant 48 heures sur la pelouse, espérant obtenir un carton rouge contre ladite bestiole" )
+fourth_article.save!
+fifth_article = Article.new(title:"Les tableaux agressés portent plainte contre les militants écologiques",
+                          content:"Depuis quelques semaines et en reaction aux attaques perpétrés contre eux,les tableaux de maitres, ont décidé de porter plainte
+                          devant la cour pénale internationale contre les organisations écologiques, afin que l'on arrête de leur lancer de la
+                          nourriture dessus.")
+fifth_article.save!
